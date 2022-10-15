abd91a2291c2f6cb69c7a18c355841e8d01e4062
Mako0722 <zhongtiancheng79@gmail.com>
2022-10-16 00:09:28 +0900

    Add 5 numbers

--- Kadai-1/ViewController.swift
+++ Kadai-1/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func didTapButton(_ sender: Any) {
+        let number1 = Int(textField1.text ?? "") ?? 0
+        let number2 = Int(textField2.text ?? "") ?? 0
+        let number3 = Int(textField3.text ?? "") ?? 0
+        let number4 = Int(textField4.text ?? "") ?? 0
+        
+        let result = number1 + number2 + number3 + number4
+        
+        label.text = "\(result)"
+    }
+    
 }
 
